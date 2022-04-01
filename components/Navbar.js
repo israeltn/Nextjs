656c77ab5f7a21953914517a1acaa5e5dde6a7c2
John Alex <85435945+JohnDalex10@users.noreply.github.com>
2022-04-01 21:33:59 +0100

    Still working on NavBar

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,15 +4,18 @@ import {Link} from "react-scroll";
 
 function Navbar() {
   return (
-    <div>
-        <nav className="shadow-sm fixed w-full z-10 flex font-pacifico">
+    <div >
+        <nav className="shadow-sm fixed w-full z-10 flex font-pacifico bg-transparent">
             <div className="w-full">
                 <div className="flex items-center h-20  w-full">
-                    <div className="flex items-center mx-20 justify-between w-full">
-                        <div className="flex justify-center items-center flex-shrink-0">
-                            <h1 className="font-bold text-xl cursor-pointer font-serif bg-red-800 h-20 text-center text-white">
+                    <div className="flex items-center mx-20 justify-between w-full h-full">
+                    <div className="h-full text-center bg-red-600 ">
+                    <h1 className="font-bold text-3xl cursor-pointer font-serif py- mx- w-48  text-white">
                                 THE FARM
                             </h1>
+                    </div>
+                        <div className="flex justify-center items-center flex-shrink-0 h-full">
+                            
                         </div>
                         <div className="hidden md:block">
                             <div className="ml-10 flex items-baseline space-x-4">
@@ -22,19 +25,39 @@ function Navbar() {
                                     smooth={true}
                                     offset={50}
                                     duration={500}
-                                    className="cursor-pointer text-red-800 font-serif font-semibold px-3 py-2 text- hover:bg-blue-600 hover:text-white transition duration-300 ease-in-out"
+                                    className="cursor-pointer text-red-800 font-serif font-semibold px-3 py-2 hover:bg-red-700 hover:text-white rounded-md transition duration-300 ease-in-out"
                                 >
-                                    Home
+                                    HOME
                                 </Link>
                                 <Link 
-                                    activeClass="About"
+                                    activeClass="ABOUT"
                                     to="about"
                                     smooth={true}
                                     offset={50}
                                     duration={500}
-                                    className="cursor-pointer text-red-800 font-serif font-semibold px-3 py-2 text- hover:bg-blue-600 hover:text-white transition duration-300 ease-in-out"
+                                    className="cursor-pointer text-red-500 font-serif font-semibold px-3 py-2 hover:bg-red-500 hover:text-white rounded-md transition duration-300 ease-in-out"
                                 >
-                                    About
+                                    ABOUT
+                                </Link>
+                                <Link 
+                                    activeClass="products"
+                                    to="products"
+                                    smooth={true}
+                                    offset={50}
+                                    duration={500}
+                                    className="cursor-pointer text-red-500 font-serif font-semibold px-3 py-2 hover:bg-red-500 hover:text-white rounded-md transition duration-300 ease-in-out"
+                                >
+                                    PRODUCTS
+                                </Link>
+                                <Link 
+                                    activeClass="contact"
+                                    to="contact"
+                                    smooth={true}
+                                    offset={50}
+                                    duration={500}
+                                    className="cursor-pointer font-serif font-semibold bg-red-500 text-white rounded-md w-28 text-center h-9"
+                                >
+                                    CONTACT
                                 </Link>
                             </div>
                         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,7 @@ module.exports = {
   ],
   theme: {
     extend: {
-      fontFamily: {
-        pacifico: 'Pacifico, cursive'
-      }
+
     },
   },
   plugins: [],
